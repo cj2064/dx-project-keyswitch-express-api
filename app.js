496e37f602efd9edf38a488a274d9e1934f44190
M_688776 <jason.monroe@cognizant.com>
2021-07-16 08:27:29 -0500

    post request and cors updates

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var developersRouter = require('./routes/developers');
 var moviesRouter = require('./routes/movies');
+
 
 var app = express();
 
@@ -17,9 +18,10 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 //adding routes to app
 app.use('/', indexRouter);
--- routes/developers.js
+++ routes/developers.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 //var mongodb = require('mongodb');
 
+let developers = [
+  { id:1, firstName: "Jason", lastName: "Monroe", favoriteLanguage:"JavaScript", yearStarted: 1999},
+  { id:2, firstName: "Bill", lastName: "Gates", favoriteLanguage:"BASIC", yearStarted: 1960}
+];
+
 /* GET developers listing. */
 router.get('/', function(req, res, next) {
-  let developer = { firstname: "Jason", lastname: "Monroe", favoriteLanguage:"JavaScript", yearStarted: 1999}
-  res.send(JSON.stringify(developer));
+  res.send(JSON.stringify(developers));
 });
 
 router.post('/developer', function(req, res, next) {
@@ -13,7 +17,9 @@ router.post('/developer', function(req, res, next) {
   let lastName = req.body.lastName;
   let favoriteLanguage = req.body.favoriteLanguage;
   let yearStarted = req.body.yearStarted;
-  console.log("Developer being posted: "+firstName+ " "+lastName);
+  let id = developers.length+1;
+  developers.push({id:id, firstName:firstName, lastName:lastName, favoriteLanguage:favoriteLanguage,yearStarted:yearStarted});
+  res.send("posted successfully");
 });
 
 module.exports = router;
