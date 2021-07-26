129820a55faeecf9695c097a9577af713f621113
Chris Jose <31542605+cj2064@users.noreply.github.com>
2021-07-26 13:56:49 -0500

    Update databaseConnection.js
    Hiding database credentials

--- mongoConfig/databaseConnection.js
+++ mongoConfig/databaseConnection.js
@@ -1,7 +1,7 @@
 // include MongoDB full driver code example here 
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://christopher:ftmg8bJ2NbjN2vH@keyswitch.anpao.mongodb.net/DXproject?retryWrites=true&w=majority";
+const uri = "mongodb+srv://christopher:<PASSWORD>@keyswitch.anpao.mongodb.net/DXproject?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // client.connect(err => {
