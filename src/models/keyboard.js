b52bafd03710e80b7af250196c2e23c01962b4f8
cj2064 <cjose@luc.edu>
2021-08-03 11:03:33 -0500

    added keyboard model back in

---
+++ src/models/keyboard.js
@@ -0,0 +1,12 @@
+class Keyboard {
+    constructor(id, name, price, keyswitches, description, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.keyswitches = keyswitches;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
+
+module.exports = Keyboard;
