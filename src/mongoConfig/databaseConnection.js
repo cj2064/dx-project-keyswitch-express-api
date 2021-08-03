a6ee0608d3d8f6b66bbb190246196dd2afd46ff9
cj2064 <cjose@luc.edu>
2021-08-03 12:11:08 -0500

    testing

--- src/mongoConfig/databaseConnection.js
+++ src/mongoConfig/databaseConnection.js
@@ -1,6 +1,7 @@
 // include MongoDB full driver code example here 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://"+process.env.MONGO_USER_PASS+"@keyswitch.anpao.mongodb.net/DXproject?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // client.connect(err => {
