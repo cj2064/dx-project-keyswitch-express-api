f7d5d5b0bba1487704ab3a11cb66ac4836d19714
cj2064 <cjose@luc.edu>
2021-08-03 11:07:35 -0500

    deleted views ref in app.js

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ const mongoConnect = require('./routes/keyboards');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
